adf2621d3302fd649cb3d4714825e283d86e7af6
ccaichilatiao <ccaichilatiao111@gmail.com>
2024-06-06 11:49:30 +0800

    implement grpc api

---
+++ gapi/convertUser.go
@@ -0,0 +1,17 @@
+package gapi
+
+import (
+	db "github.com/ccaichilatiao/simplebank/db/sqlc"
+	"github.com/ccaichilatiao/simplebank/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
